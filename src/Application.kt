28415b330860cd317ecdef71a9e5a86b29795456
Shengyou Fan <shengyoufan@gmail.com>
2020-08-24 00:56:46 +0800

    add connection string

--- src/Application.kt
+++ src/Application.kt
@@ -4,12 +4,19 @@ import io.ktor.application.*
 import io.ktor.response.*
 import io.ktor.request.*
 import io.ktor.routing.*
+import org.jetbrains.exposed.sql.Database
 
 fun main(args: Array<String>): Unit = io.ktor.server.netty.EngineMain.main(args)
 
 @Suppress("unused") // Referenced in application.conf
 @kotlin.jvm.JvmOverloads
 fun Application.module(testing: Boolean = false) {
+
+    // 先連線到 H2
+    Database.connect(
+        url = "jdbc:h2:mem:test;DB_CLOSE_DELAY=-1",
+        driver = "org.h2.Driver"
+    )
 
     routing {
 
